8cf2598c54fd7f230cd4e26ff07619239b412d2a
Zilong <106037081+Zilong007z@users.noreply.github.com>
2023-04-23 01:27:26 +0300

    done

---
+++ ReadMe.md
@@ -0,0 +1,13 @@
+<h1>Test Task</h1>
+
+<h3>This is for windows users</h3>
+
+<h2>first steps to use this program</h2>
+
+<h3>1.git clone https://github.com/DmitroPodolsky/test_task.git</h3>
+
+<h3>2.cd test_task</h3>
+
+<h3>3.pip install requirements.txt</h3>
+
+<h4>after that run "python main.py" to get result in console</h4>
--- main.py
+++ main.py
@@ -1,33 +1,41 @@
-from selenium import webdriver # Подключаем библиотеку
+from selenium import webdriver # driver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
-def end(n):
-    if n == 5:
-        return True
-    return False
-def start():
-    driver = webdriver.Chrome(executable_path ="ChromeSetup(2).exe")
-    driver.get('https://www.google.com/')
-    search = driver.find_element(By.XPATH, '//*[@id="APjFqb"]')
-    search.send_keys('Python Developer')
-    time.sleep(3)
-    search.send_keys(Keys.ENTER)
-    n=0
-    for i in range(len(driver.find_elements(By.CLASS_NAME,'uEierd'))):
-        try:
-            print(driver.find_element(By.XPATH,f'//div[@id="tads"]/div[{i+1}]/div/div/div/div[1]/a/div[1]/span').text)
-        except:
-            n-=1
-        n+=1
-        if end(n):
-            return
-    for i in range(len(driver.find_elements(By.CLASS_NAME,'MjjYud'))):
-        try:
-            print(driver.find_element(By.XPATH,f'//*[@id="rso"]/div[{i+1}]/div/div/div[1]/div/a/h3').text)
-        except:
-            n-=1
-        n += 1
-        if end(n):
-            return
-start()
+
+
+class Runner():
+    """returns 5 firsts names links"""
+    def __init__(self):
+        self.start()
+    @staticmethod
+    def end(minimal_row_output) -> int:
+        return minimal_row_output == 5
+
+    def start(self) -> None:
+        driver = webdriver.Chrome(executable_path="ChromeSetup(2).exe")
+        driver.get('https://www.google.com/')
+        search = driver.find_element(By.XPATH, '//*[@id="APjFqb"]')
+        search.send_keys('Python Developer')
+        search.send_keys(Keys.ENTER)
+        minimal_row_output = 0
+        for i in range(len(driver.find_elements(By.CLASS_NAME, 'uEierd'))):
+            try:
+                print(driver.find_element(By.XPATH,
+                                          f'//div[@id="tads"]/div[{i + 1}]/div/div/div/div[1]/a/div[1]/span').text)
+            except Exception:
+                minimal_row_output -= 1
+            minimal_row_output += 1
+            if self.end(minimal_row_output):
+                return
+        for i in range(len(driver.find_elements(By.CLASS_NAME, 'MjjYud'))):
+            try:
+                print(driver.find_element(By.XPATH, f'//*[@id="rso"]/div[{i + 1}]/div/div/div[1]/div/a/h3').text)
+            except Exception:
+                minimal_row_output -= 1
+            minimal_row_output += 1
+            if self.end(minimal_row_output):
+                return
+
+
+if __name__ == "__main__":
+    Runner()
